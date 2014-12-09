76eddb43bd4f6fc3e8eb3dfb7aa42d5d4ec920cc
arkye <arkye@hotmail.com.br>
2014-12-09 19:33:22 -0200

    Updating Algebra Library

--- algebra.h
+++ algebra.h
@@ -7,10 +7,18 @@ typedef enum Boolean
 	TRUE
 }Boolean;
 
+typedef struct Two_Natural_Numbers
+{
+	unsigned int first_number;
+	unsigned int second_number;
+	Two_Natural_Numbers(){first_number = 0, second_number = 0;}
+}Two_Natural_Numbers;
+
 Boolean is_odd(int input_number);
+Boolean is_negative(int input_number);
 
-unsigned int random_positive_number(unsigned int minimum_number, unsigned int maximum_number);
+unsigned int random_natural_number(unsigned int minimum_number, unsigned int maximum_number);
 
-unsigned int[2] decompose_as_power_of_two(unsigned int input_number);
+Two_Natural_Numbers decompose_as_power_of_two(unsigned int input_number);
 
 #endif // ALGEBRA_H_
