35668ea2956842e796c1fc59152e2de786e376e6
Phelps <phwener@gmail.com>
2014-12-09 19:38:02 -0200

    Implementação de is_odd e decompose

---
+++ algebra.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "algebra.h"
+
+/*
+* Verifica se um numero é impar
+*/
+Boolean is_odd(int input_number)
+{
+	if(input_number % 2 == 0){
+		return FALSE;
+	}
+	else
+	{
+		return TRUE;
+	}
+}
+
+/*
+*
+*/
+unsigned int random_positive_number(unsigned int minimum_number, unsigned int maximum_number){
+	
+}
+
+/*
+* Tenta achar dois numeros d e s tais que  n-1 = 2^s * d, onde d é impar
+* Retorna um vetor de inteiros contendo s e d respectivamente
+*/
+unsigned int[2] decompose_as_power_of_two(unsigned int input_number){
+	int s, d;
+	int factors[2];
+	for(s = 1; s < input_number; s++){
+		for(d = 1; d < input_number/s; d++){
+			int result = (int) pow(2, s)*d;
+			if(result == input_number && is_odd(d) == TRUE){
+				factors[0] = s;
+				factors[1] = d;
+                                return factors;
+                         }
+                 }       
+         }  
+}
