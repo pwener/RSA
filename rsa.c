f8a777f510250fa57a0048b78266a34ab5f32881
pedrodelyra <pedrodelyra@gmail.com>
2014-12-11 18:04:20 -0200

    Algoritmo RSA, implementação do calculo do totiente de euler, da busca do coprimo, do MDC, da busca do inverso multiplicativo de n(mod(totiente))

--- rsa.c
+++ rsa.c
@@ -1,11 +1,28 @@
 #include "msr.h"
 #include <time.h>
 
+typedef struct public_key {
+	int rsa_modulus;
+	int coprime;
+} Public_Key;
+
+typedef struct private_key {
+	int rsa_modulus;
+	int modular_multiplicative_inverse;
+} Private_Key;
+
 void rsa(char* text, int first_prime, int second_prime)
 {
 	int rsa_modulus = first_prime * second_prime;
 	int euler_totient = (first_prime - 1) * (second_prime - 1);
-		
+	int coprime = find_coprime(euler_totient);
+	int modular_multiplicative_inverse = find_modular_multiplicative_inverse(coprime, euler_totient);		
+	Public_Key public_key = malloc(sizeof(Public_Key));
+	Private_Key private_key = malloc(sizeof(Private_Key));
+	public_key->rsa_modulus = rsa_modulus;	
+	public_key->coprime = coprime;
+	private_key->rsa_modulus = rsa_modulus;
+	private_key->modular_multiplicative_inverse = modular_multiplicative_inverse;		
 }
 
 int read_number() 
@@ -40,7 +57,7 @@ int great_common_divisor(int first_number, int second_number)
 		return great_common_divisor(second_number, first_number % second_number);
 }
 
-int modular_multiplicative_inverse(int coprime, int euler_totiente) {
+int find_modular_multiplicative_inverse(int coprime, int euler_totiente) {
 	int modular_inverse = 0;	
 	for (int i = 1; i < euler_totiente; i++) {
 		if ((i * coprime - 1) % euler_totiente == 0) {
@@ -49,5 +66,13 @@ int modular_multiplicative_inverse(int coprime, int euler_totiente) {
 		}
 	}
 	return modular_inverse;
+}
+
+void encrypt(char* pure_text, Public_Key key) {
+	
+}
+
+void decrypt(char* encrypted_text, Private_Key key) {
+	
 }
 
